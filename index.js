a470c6038c6867e3c0a250aa95f2059f36f1e7ad
Mejbah <mejba.s@gmail.com>
2022-05-27 06:58:24 +0600

    purchase data successfully loaded in mongobd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port =process.env.PORT || 5000;
@@ -10,7 +10,61 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orz1c.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('tools_house').collection('services');
+    const reviewCollection = client.db('tools_house').collection('reviews');
+    const purchaseCollection = client.db('tools_house').collection('purchases');
+    
+    // Reviews
+    app.get('/review', async(req, res)=>{
+      const query ={};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.json(reviews);
+    })
+    
+    
+    // Tools/Services
+    app.get('/service', async(req, res)=>{
+      const query ={};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.json(services);
+    })
+    
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.json(result);
+  });
+  
+  app.post("/purchase", async (req, res)=>{
+    const purchase = req.body;
+    const query = {toolsName: purchase.toolsName, userEmail: purchase.userEmail}
+    const exists = await purchaseCollection.findOne(query);
+    if (exists) {
+      return res.json({success: false, purchase: exists})
+    }
+    const result = await purchaseCollection.insertOne(purchase);
+    return res.json({success: true, result});
+  })
+    
+  }
+  finally {
+    
+  }
+  
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello from Tools House!')
