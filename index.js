ddd4ed29ff58e589283c84aa52f815a2e0a853db
Mejbah <mejba.s@gmail.com>
2022-05-28 04:45:40 +0600

    verify token implemented

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'UnAuthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 async function run() {
   try {
@@ -91,8 +106,9 @@ async function run() {
   //   res.json(purchases);
   // })
   
-  app.get('/purchase', async (req, res) =>{
-    const userEmail = req.query.userEmail;
+  app.get('/purchase', verifyJWT, async (req, res) =>{
+    const userEmail = req.query.userEmail;    
+    console.log('auth headers', authorization)
     const query ={userEmail: userEmail}
     const purchases = await purchaseCollection.find(query).toArray();
     res.json(purchases);
